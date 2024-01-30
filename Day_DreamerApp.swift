8b13f5d0bfccbfdbe931acc4e4ba4781a38c4ee6
Adam <adam.strickland@stricktech.com>
2024-01-30 18:10:25 -0500

    test

---
+++ Day_DreamerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Day_DreamerApp.swift
+//  Day Dreamer
+//
+//  Created by Adam Strickland on 1/30/24.
+//
+
+import SwiftUI
+
+@main
+struct Day_DreamerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
