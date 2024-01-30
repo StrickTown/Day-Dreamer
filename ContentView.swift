594e3801d9b4ee4922f54367a499a0d29c743be8
Adam <adam.strickland@stricktech.com>
2024-01-30 18:12:47 -0500

    Test - Final

--- ContentView.swift
+++ ContentView.swift
@@ -15,6 +15,7 @@ struct ContentView: View {
                 .foregroundStyle(.tint)
             Text("Hello, world!")
             Text("Test")
+            Text("Houston, we're home")
         }
         .padding()
     }
