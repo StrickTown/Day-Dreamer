f8a00afeebb96ee2fd8eab4bae6cc341e79634e1
Adam <adam.strickland@stricktech.com>
2024-02-10 08:42:27 -0500

    Updated home screen and day dream card

--- Component/DayDreamCard.swift
+++ Component/DayDreamCard.swift
@@ -7,17 +7,72 @@
 
 import SwiftUI
 
-struct DayDreamCard: View {
-    var body: some View {
-        Text("Hello, World!")
-            .padding() // Add padding around the text
-            .frame(width: 310, height: 200)
-            .background(Color("SecondaryBackground")) // Set the card's background color to white
-            .cornerRadius(10) // Round the corners of the card
-            .shadow(radius: 5) // Apply a shadow with a radius of 5 to create the elevation effect
-//            .padding() // Add more padding to separate the card from the edges of the screen
-    }
+struct DayDreamCard: View{
+    
+    var body: some View{
         
+        let width = UIScreen.main.bounds.width
+        
+        ZStack{
+            
+            RoundedRectangle(cornerRadius: 25)
+                .fill(.white)
+                .opacity(0.08)
+                .background(
+                
+                    Color.white
+                        .opacity(0.008)
+                        .blur(radius: 10)
+                )
+            // Strokes...
+                .background(
+                
+                    RoundedRectangle(cornerRadius: 25)
+                        .stroke(
+                        
+                            .linearGradient(.init(colors: [
+                            
+                                Color("Primary"),
+                                Color.purple.opacity(0.5),
+                                .clear,
+                                .clear,
+                                Color("Secondary"),
+                            ]), startPoint: .topLeading, endPoint: .bottomTrailing)
+                            ,lineWidth: 2.5
+                        )
+                        .padding(2)
+                )
+            // Shadows...
+                .shadow(color: .black.opacity(0.1), radius: 5, x: -5, y: -5)
+                .shadow(color: .black.opacity(0.1), radius: 5, x: 5, y: 5)
+            
+            // COntent...
+            VStack{
+                
+                Image(systemName: "sun.max")
+                    .font(.system(size: 45, weight: .thin))
+                
+                Text("18")
+                    .font(.system(size: 85, weight: .bold))
+                    .padding(.top,2)
+                    .overlay(
+                    
+                        Text("Days")
+                            .font(.title2)
+                            .foregroundColor(Color.white.opacity(0.7))
+                            .offset(x: 50, y: 15)
+                        
+                        ,alignment: .topTrailing
+                    )
+                    .offset(x: -6)
+                
+                Text("Day Dream")
+                    .font(.title3)
+                    .foregroundColor(Color.white.opacity(0.4))
+            }
+        }
+        .frame(width: 320, height: 270)
+    }
 }
 
 struct DayDreamCard_Previews: PreviewProvider {
--- Screens/HomeScreen.swift
+++ Screens/HomeScreen.swift
@@ -6,20 +6,127 @@
 //
 import SwiftUI
 
-struct HomeScreen: View {
-    var body: some View {
-        Color("PrimaryBackground")
-            .edgesIgnoringSafeArea(.all)
-            .overlay(
-                ScrollView(showsIndicators: false) { // Enables scrolling
-                    VStack(spacing: 20) { // Vertically arranges your cards with spacing
-                        ForEach(0..<10) { _ in // Example loop to create multiple cards
-                            DayDreamCard()
+//struct HomeScreen: View {
+//    var body: some View {
+//        Color("PrimaryBackground")
+//            .edgesIgnoringSafeArea(.all)
+//            .overlay(
+//                ScrollView(showsIndicators: false) { // Enables scrolling
+//                    VStack(spacing: 20) { // Vertically arranges your cards with spacing
+//                        ForEach(0..<10) { _ in // Example loop to create multiple cards
+//                            DayDreamCard()
+//                        }
+//                    }
+//                    .padding() // Adds padding around the VStack content
+//                }
+//            )
+//    }
+//}
+
+
+struct HomeScreen: View{
+    
+    var body: some View{
+        
+        ZStack{
+            
+            LinearGradient(
+                colors: [Color("Primary"),Color("Secondary")],
+                startPoint: .top,
+                endPoint: .bottom)
+                .ignoresSafeArea()
+            
+            // Gloss Background....
+            GeometryReader{proxy in
+                
+                let size = proxy.size
+                
+                // Slighlty Darkening ...
+                Color.black
+                    .opacity(0.7)
+                    .blur(radius: 200)
+                    .ignoresSafeArea()
+                
+                Circle()
+                    .fill(Color("Purple"))
+                    .padding(50)
+                    .blur(radius: 120)
+                // Moving Top...
+                    .offset(x: -size.width / 1.8, y: -size.height / 5)
+                
+                Circle()
+                    .fill(Color("LightBlue"))
+                    .padding(50)
+                    .blur(radius: 150)
+                // Moving Top...
+                    .offset(x: size.width / 1.8, y: -size.height / 2)
+                
+                
+                Circle()
+                    .fill(Color("LightBlue"))
+                    .padding(50)
+                    .blur(radius: 90)
+                // Moving Top...
+                    .offset(x: size.width / 1.8, y: size.height / 2)
+                
+                // Adding Purple on both botom ends...
+                
+                Circle()
+                    .fill(Color("Purple"))
+                    .padding(100)
+                    .blur(radius: 110)
+                // Moving Top...
+                    .offset(x: size.width / 1.8, y: size.height / 2)
+                
+                Circle()
+                    .fill(Color("Purple"))
+                    .padding(100)
+                    .blur(radius: 110)
+                // Moving Top...
+                    .offset(x: -size.width / 1.8, y: size.height / 2)
+            }
+            
+            // Content....
+            VStack{
+                
+//                Spacer(minLength: 10)
+                
+                // GlassMorphism Card...
+                
+                ZStack{
+                    
+                    
+                    // Bacground Balls...
+                    Circle()
+                        .fill(Color("Purple"))
+                        .blur(radius: 20)
+                        .frame(width: 100, height: 100)
+                        .offset(x: 120, y: -80)
+                    
+                    Circle()
+                        .fill(Color("LightBlue"))
+                        .blur(radius: 40)
+                        .frame(width: 100, height: 100)
+                        .offset(x: -120, y: 100)
+                    
+                    ScrollView(showsIndicators: false) { // Enables scrolling
+                        VStack(spacing: 20) { // Vertically arranges your cards with spacing
+                            ForEach(0..<10) { _ in // Example loop to create multiple cards
+                                DayDreamCard()
+                            }
                         }
+                        .padding() // Adds padding around the VStack content
                     }
-                    .padding() // Adds padding around the VStack content
                 }
-            )
+                
+//                Spacer(minLength: 10)
+
+
+            }
+            .foregroundColor(.white)
+            .multilineTextAlignment(.center)
+//            .padding()
+        }
     }
 }
 
@@ -28,3 +135,4 @@ struct HomeScreen_Previews: PreviewProvider {
         HomeScreen()
     }
 }
+
